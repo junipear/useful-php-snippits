3cb0d872daead970252569429355a651b75db4e9
CC <43211030+junipear@users.noreply.github.com>
2018-12-03 14:06:40 -0700

    Create detect-ajax-request.php

---
+++ detect-ajax-request.php
@@ -0,0 +1,7 @@
+<?php
+if(!emptyempty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'){  
+    //If AJAX Request Then  
+}else{  
+//something else  
+}  
+?>
